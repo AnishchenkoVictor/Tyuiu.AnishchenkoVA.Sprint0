600d445238140242cb441d834ab69e058328690b
Viktor Anishchenko <anishchenko.vitya@inbox.ru>
2024-09-20 20:28:51 +0500

    Task3

---
+++ Tyuiu.AnishchenkoVA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.AnishchenkoVA.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.AnishchenkoVA.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+        }
+    }
+}
